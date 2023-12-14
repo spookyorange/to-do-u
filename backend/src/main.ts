19400b8491bbaf6f20a593ffeb48cef1241f472c
spookyorange <korkmazcoltu@mail.ru>
2023-12-14 17:51:29 +0300

    remove cats from tags

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
     .setTitle('Todo-U API Documentation')
     .setDescription('The good stuff starts here!')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
